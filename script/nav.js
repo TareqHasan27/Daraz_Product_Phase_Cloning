332cc50370bdaa27d366d40ea1598a0f35efaf12
Tareq hasan <tareqhasan1201@gmail.com>
2025-01-10 15:41:20 +0600

    responsive header

---
+++ script/nav.js
@@ -0,0 +1,9 @@
+
+    const menuToggle = document.getElementById('menuToggle');
+    const navMenu = document.getElementById('navMenu');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('hidden');
+        navMenu.classList.toggle('block');
+    });
+
